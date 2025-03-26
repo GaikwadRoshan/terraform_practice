3b73ea8822032b33236287d7039418d64e5eb078
GaikwadRoshan <gaikwadroshan27@gmail.com>
2025-03-26 18:23:16 +0530

    adding

--- aws_ec2/main.tf
+++ aws_ec2/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "demo" {
-    ami = "var.ami_id"
+    ami = var.ami_id
     instance_type = var.instance_type
     security_groups = [aws_security_group.tf_sf.name]
     tags = var.instance_tag
