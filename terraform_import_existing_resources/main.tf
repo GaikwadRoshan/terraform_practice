b7df3f7ee70b5b18b0b7e843d21ceadb0a2a9ddc
GaikwadRoshan <gaikwadroshan27@gmail.com>
2025-07-25 16:51:26 +0530

    adding correction

--- terraform_import_existing_resources/main.tf
+++ terraform_import_existing_resources/main.tf
@@ -6,5 +6,10 @@ region = "ap-south-1"
 }
 
 resource "aws_instance" "main" {
-    
+    ami = "ami-0f918f7e67a3323f0"
+    instance_type = "t2.micro"
+    tags = {
+        name = "tf-server"
+    }
+
 }
