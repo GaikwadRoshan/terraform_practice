d3bf4cb44532ea4dacf4bf8c7c7ea4511447e69f
GaikwadRoshan <gaikwadroshan27@gmail.com>
2025-07-25 17:24:26 +0530

    adding_final_code_to_import

--- terraform_import_existing_resources/main.tf
+++ terraform_import_existing_resources/main.tf
@@ -3,13 +3,19 @@ provider aws {
 # Configuration options
 
 region = "ap-south-1"
+#access_key = 
+#secret_key = 
 }
 
 resource "aws_instance" "main" {
-    ami = "ami-0f918f7e67a3323f0"
-    instance_type = "t2.micro"
-    tags = {
-        name = "tf-server"
-    }
+
+    #run import command in terminal "terraform import aws_instance.main <your_instance_id>"
+    #after import successfull msg give below required details and then terrafoem plan and terraform apply
+
+   # ami = "ami-0f918f7e67a3323f0"
+    #instance_type = "t2.micro"
+    #tags = {
+        #Name = "tf-server"
+    #}
 
 }
